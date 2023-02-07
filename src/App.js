ef8ff57e252dc7245ab46103914fcdd117b95363
benhmidawael <benhmidawael97@gmail.com>
2023-02-07 15:47:26 +0100

    hh

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
        <Profile fullName="Ben Hmida Wael"
            bio="Hi there !"
-           profession="Mana5demch !"
+           profession="Student"
            handleName={handleName}
            >
         <div>
